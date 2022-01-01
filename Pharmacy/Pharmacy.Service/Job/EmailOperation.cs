83ac73b2d7f02426d94e60b8769836034b54edc0
feyzakeless <feyzakeles@gmail.com>
2022-01-02 00:02:00 +0300

    Created service for email

--- Pharmacy/Pharmacy.Service/Job/EmailOperation.cs
+++ Pharmacy/Pharmacy.Service/Job/EmailOperation.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using MailKit.Net.Smtp;
+using MimeKit;
 using Pharmacy.DB.Entities.DataContext;
 using Pharmacy.Model;
 using Pharmacy.Model.ModelUser;
@@ -10,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Pharmacy.Service.Job
 {
-    public class EmailOperation
+    public class EmailOperation :IEmailOperation
     {
         private readonly IMapper mapper; //Mapper çagrildi
 
@@ -28,9 +30,26 @@ namespace Pharmacy.Service.Job
 
                 if (!user.IsSendEmail)
                 {
+
+                    var message = new MimeMessage();
+                    message.From.Add(new MailboxAddress("Pharmacy", "info@pharmacy.com"));
+                    message.To.Add(new MailboxAddress("User", user.Email));
+                    message.Subject = "Welcome to the pharmacy";
+                    message.Body = new TextPart("plain")
+                    {
+                        Text = "Dear"+user.Name+ ", Welcome To the Pharmacy!"
+                    };
+
+                    using (var client = new SmtpClient())
+                    {
+                        client.Connect("smtp@gmail.com", 587, false);
+                        client.Authenticate("info@pharmacy.com", "visualstudio");
+                        client.Send(message);
+                        client.Disconnect(true);
+                    }
+
                     user.IsSendEmail = true;
                     context.SaveChanges();
-
                     result.Entity = mapper.Map<UserViewModel>(user);
 
                     result.IsSuccess = true;
