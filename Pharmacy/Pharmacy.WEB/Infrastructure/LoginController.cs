d015e3ce8524afded60f1c840a2a158f5befedec
feyzakeless <feyzakeles@gmail.com>
2022-01-02 00:01:29 +0300

    Created web layer

---
+++ Pharmacy/Pharmacy.WEB/Infrastructure/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pharmacy.WEB.Infrastructure
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
